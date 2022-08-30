d9bbf7acec32b51c4ee688952958ae2d69c88fc6
Emmanuel Fermin <emmanuelfermin1990@gmail.com>
2022-08-30 14:55:12 +0800

    Button and Spacings: Adjusted and styled a bit

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from "react";
 import { IUIButtonProps } from "../types/ui";
+import classes from "./Button.module.css"
 
 const Button: FC<IUIButtonProps> = ({ id, onClick, title }): JSX.Element => {
   return (
-    <button id={id} onClick={onClick}>
+    <button className={classes.btn} id={id} onClick={onClick}>
       {title}
     </button>
   );
